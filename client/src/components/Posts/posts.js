df815f99640c4d5ea6219e0312a7c2b85a90445c
vansia <dh608661@dal.ca>
2022-05-26 08:33:44 -0300

    Display post

--- client/src/components/Posts/posts.js
+++ client/src/components/Posts/posts.js
@@ -16,9 +16,8 @@ const Posts   = () => {
             !posts.length ? <CircularProgress/> : (
                 <Grid className={classes.container} container alignItems='stretch' spacing='3' >
                     {posts.map((post) => (
-                        <Grid key={post._id} item xs={12} xm={6}>
+                        <Grid key={post._id} item xs={12} sm={6} md={6}>
                             <Post post={post} />
-
                         </Grid>
                     ))}
                 </Grid>
